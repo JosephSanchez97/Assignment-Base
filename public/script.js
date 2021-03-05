217c68d207dbebc23b6c27e5668aae69db941356
abhipad12 <abhishekpad12@gmail.com>
2021-03-05 16:02:19 -0500

    Update script.js

--- public/script.js
+++ public/script.js
@@ -2,6 +2,7 @@ async function windowActions() {
     console.log('Window loaded');
     const form = document.querySelector('.field');
     const search = document.querySelector('#search');
+    const suggestions = document.querySelector('.suggestions');
 
     const request = await fetch('/api');
     const data = await request.json();
@@ -17,9 +18,24 @@ async function windowActions() {
         });
 
     search.addEventListener('input', (event) => {
-        console.log('input', event.target.value);
-    });
-}
+        console.log('input', event.target.value)
+        const display = data.filter((record) => {
+            return record.city.toUpperCase().includes(event.target.value.toUpperCase()) || record.zip.includes(event.target.value);
+        });
+display.forEach(restaurant => {
+    const newItem = document.createElement('li');
+    newItem.classList.add('list-item');
+    newItem.innerHTML = `
+    <strong>${restaurant.name}</strong>
+    ${restaurant.category}
+    ${restaurant.address_line_1}
+    ${restaurant.city}
+    ${restaurant.zip}
+    `;
+    suggestions.append(newItem);
+        });
+})
+};
 window.onload = windowActions;
 
         //const request = await fetch('/api', {
