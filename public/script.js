529f6cb17bb066a046d0b6429565eb4b9a85a240
Joseph Sanchez <sanchezjoseph378@gmail.com>
2021-03-04 21:24:46 -0500

    ADD: HTML added script to bottom of page. JS: window onload

--- public/script.js
+++ public/script.js
@@ -1,3 +1,15 @@
+async function windowActions() {
+    console.log('Window loaded');
+    const form = document.querySelector('.field');
+    const search = document.querySelector('#search');
+    const request = await fetch('/api');
+    const data = await request.json();
+    console.table(data);
+    form.addEventListener('input', async (event) => {
+        console.log('input', event.target.value)
+    });
+}
+/*
 const endpoint = https://data.princegeorgescountymd.gov/resource/umjn-t2iz.json
 
 const zip = []
@@ -12,3 +24,4 @@ function findMatches(zipToMatch, zip) {
         return place.zip.match(regex)
     });
 }
+*/ 
