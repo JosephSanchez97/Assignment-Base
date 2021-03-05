15617e877736b2b30c25f30f80eea10da9affdd6
Joseph Sanchez <sanchezjoseph378@gmail.com>
2021-03-04 23:26:20 -0500

    ADD: Windowload function, event listener for content and input

--- public/script.js
+++ public/script.js
@@ -2,26 +2,33 @@ async function windowActions() {
     console.log('Window loaded');
     const form = document.querySelector('.field');
     const search = document.querySelector('#search');
+
     const request = await fetch('/api');
     const data = await request.json();
-    console.table(data);
-    form.addEventListener('input', async (event) => {
-        console.log('input', event.target.value)
+    
+    form.addEventListener('submit', async (event) => {
+        event.preventDefault();
+        console.log('submit fired', search.value);
+        const display = data.filter((record) => 
+            record.zip === search.value);
+
+            console.log(display);
+            console.table(display);
+        });
+
+    search.addEventListener('input', (event) => {
+        console.log('input', event.target.value);
     });
 }
-/*
-const endpoint = https://data.princegeorgescountymd.gov/resource/umjn-t2iz.json
+window.onload = windowActions;
 
-const zip = []
-
-fetch(endpoint)
-    .then(blob => blob.json())
-    .then(data => zip.push(...data))
-
-function findMatches(zipToMatch, zip) {
-    return zip.filter(place => {
-        const regex = new RegExp(zipToMatch, 'gi');
-        return place.zip.match(regex)
-    });
-}
-*/ 
+        //const request = await fetch('/api', {
+          //  method: 'POST',
+            //headers: {
+              //  'Content-Type': 'application/json'
+        //},
+        //body: JSON.stringify({ data: search.value})
+  //      });
+//        const data = await request.json();
+    //    console.log(data);
+        //console.table(data);
