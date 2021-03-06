c642576fbde0bbcbc866392b8736bbbb77123b17
Joseph Sanchez <sanchezjoseph378@gmail.com>
2021-03-05 23:05:11 -0500

    Prettier and removed code that doesn't apply from JS

--- public/script.js
+++ public/script.js
@@ -103,37 +103,34 @@ searchInput.addEventListener('keyup', displayMatches);
 window.onload = windowActions; */
 
 async function windowActions() {
-    const search = document.querySelector("#search");
-    //const searchByName = document.querySelector("#name");
-    //const searchByFoodType = document.querySelector("#foodType");
-    //const searchByZipCode = document.querySelector("#zipCode");
-    const filteredList = document.querySelector("#filteredList");
-  
-    let filteredPlaces = [];
-    let searchType = "name";
-  
-    function findMatches(wordToMatch, places) {
-      return places.filter(place => {
-        const regex = new RegExp(wordToMatch, "gi");
-        return place.name.match(regex);
-      });
+  const search = document.querySelector("#search");
+  const filteredList = document.querySelector("#filteredList");
+
+  let filteredPlaces = [];
+  let searchType = "name";
+
+  function findMatches(wordToMatch, places) {
+    return places.filter((place) => {
+      const regex = new RegExp(wordToMatch, "gi");
+      return place.name.match(regex);
+    });
+  }
+  function removeChildren(parent) {
+    while (parent.firstChild) {
+      parent.removeChild(parent.firstChild);
     }
-    function removeChildren(parent) {
-      while (parent.firstChild) {
-        parent.removeChild(parent.firstChild);
-      }
-    }
-  
-    function displayMatch() {
-      fetch("/api")
-        .then(res => res.json())
-        .then(json => {
-          filteredPlaces = findMatches(search.value, json);
-          removeChildren(filteredList);
-          filteredPlaces.forEach(place => {
-            filteredList.insertAdjacentHTML(
-              "beforeend",
-              `<li class='card mt-4'>
+  }
+
+  function displayMatch() {
+    fetch("/api")
+      .then((res) => res.json())
+      .then((json) => {
+        filteredPlaces = findMatches(search.value, json);
+        removeChildren(filteredList);
+        filteredPlaces.forEach((place) => {
+          filteredList.insertAdjacentHTML(
+            "beforeend",
+            `<li class='card mt-4'>
                 <div class="card-content">
                     <div class="content">
                         <p class="title is-3">${place.name}</p>
@@ -143,13 +140,12 @@ async function windowActions() {
                     </div>
                 </div>
                 </li>`
-            );
-          });
+          );
         });
-    }
-  
-    search.addEventListener("change", displayMatch);
-    search.addEventListener("keyup", displayMatch);
-  //  radio.addEventListener("change", displayMatch);
+      });
   }
-  window.onload = windowActions;
+
+  search.addEventListener("change", displayMatch);
+  search.addEventListener("keyup", displayMatch);
+}
+window.onload = windowActions;
