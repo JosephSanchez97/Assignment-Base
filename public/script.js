f851a01dd58eba064321936977c43a87a6b0413d
Joseph Sanchez <sanchezjoseph378@gmail.com>
2021-03-09 13:38:12 -0500

    EDIT: Fixed Functionalality of JS HTML and CSS

--- public/script.js
+++ public/script.js
@@ -1,117 +1,15 @@
-/*async function windowActions() {
-    console.log('Window loaded');
-    const form = document.querySelector('.field');
-    const search = document.querySelector('#search');
-    const suggestions = document.querySelector('.suggestions');
-
-    const request = await fetch(endpoint);
-    const data = await request.json();
-    
-    form.addEventListener('submit', async (event) => {
-        event.preventDefault();
-        console.log('submit fired', search.value);
-        const display = data.filter((record) => 
-            record.zip === search.value);
-
-            console.log(display);
-            console.table(display);
-        });
-
-    search.addEventListener('input', (event) => {
-        console.log('input', event.target.value)
-        const display = data.filter((record) => {
-            return record.city.toUpperCase().includes(event.target.value.toUpperCase()) || record.zip.includes(event.target.value);
-        });
-display.forEach(restaurant => {
-    const newItem = document.createElement('li');
-    newItem.classList.add('list-item');
-    newItem.innerHTML = `
-    <strong>${restaurant.name}</strong>
-    ${restaurant.category}
-    ${restaurant.address_line_1}
-    ${restaurant.city}
-    ${restaurant.zip}
-    `;
-    suggestions.append(newItem);
-        });
-})
-};
-window.onload = windowActions;
-
-        //const request = await fetch('/api', {
-          //  method: 'POST',
-            //headers: {
-              //  'Content-Type': 'application/json'
-        //},
-        //body: JSON.stringify({ data: search.value})
-  //      });
-//        const data = await request.json();
-    //    console.log(data);
-        //console.table(data);
-
-*/
-/* async function windowActions() {
-const endpoint = 'https://data.princegeorgescountymd.gov/resource/umjn-t2iz.json';
-
-const zipcodes = [];
-
-fetch(endpoint)
-    .then(blob => blob.json())
-    .then(data => zipcodes.push(...data))
-
-function findMatches(wordToMatch, zipcodes) {
-    return zipcodes.filter(restaurant => {
-        const regex = new RegExp(wordToMatch, 'gi');
-        return restaurant.zipcode.match(regex)
-    });
-
-}
-
-function displayMatches() {
-    //console.log(this.value);
-    const matchArray = findMatches(this.value, zipcodes);
-    //console.log(matchArray)
-    const html = matchArray.map(restaurant => {
-        const restaurantName = restaurant.name;
-        const addressLine1 = restaurant.address_line_1;
-        const addressLine2 = restaurant.address_line_2;
-        const [category] = restaurant;
-
-        return `
-        <div class = "box is-medium"> 
-        <li> 
-            <div class = "name"> ${restaurantName} </div>
-            <div class = "address"> ${addressLine1} </div>
-            <div class = "address"> ${addressLine2} </div>
-            <div class = "category"> ${category} </div>
-
-        </li>
-        </div>
-        `;
-    }).join('');
-    suggestions.innerHTML = html;
-}
-
-//const form = document.querySelector('.field');
-const searchInput = document.querySelector('.search');
-const suggestions = document.querySelector('.suggestions');
-
-searchInput.addEventListener('input', displayMatches);
-searchInput.addEventListener('keyup', displayMatches);
-}
-
-window.onload = windowActions; */
-
 async function windowActions() {
-  const search = document.querySelector("#zip");
-  const filteredList = document.querySelector("#filteredList");
-
-  let filteredPlaces = [];
+  const search = document.querySelector('#searchByZip');
+  const submitButton = document.querySelector('#subButton');
+  const filteredList = document.querySelector('#filteredList');
+  const endpoint = 'https://data.princegeorgescountymd.gov/resource/umjn-t2iz.json'
+  const request = await fetch(endpoint);
+  const locations = await request.json();
 
 
   function findMatches(search, places) {
     return places.filter(place => {
-        const regex = new RegExp(search, "gi");
+        const regex = new RegExp(search, 'gi')
         return place.zip.match(regex)
     });
 }
@@ -123,11 +21,8 @@ async function windowActions() {
     }
   }
 
-  function displayMatch() {
-    fetch("/api")
-      .then((res) => res.json())
-      .then((json) => {
-        filteredPlaces = findMatches(search.value, json);
+  async function displayMatches() {
+        const filteredPlaces = findMatches(search.value, locations);
         removeChildren(filteredList);
         filteredPlaces.forEach((place) => {
           filteredList.insertAdjacentHTML(
@@ -141,13 +36,12 @@ async function windowActions() {
                             ${place.city}, ${place.state}. ${place.zip}</address>
                     </div>
                 </div>
-                </li>`
-          );
-        });
-      });
+                </li>`)
+        })
   }
 
-  search.addEventListener("change", displayMatch);
-  search.addEventListener("keyup", displayMatch);
+  search.addEventListener("change", displayMatches);
+  search.addEventListener("keyup", displayMatches);
+  //submitButton.addEventListener("onsubmit", displayMatches);
 }
 window.onload = windowActions;
